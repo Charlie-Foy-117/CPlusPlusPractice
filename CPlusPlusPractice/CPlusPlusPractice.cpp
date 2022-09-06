88dbc5a171985382cb5f97fc2a6f4a7e5c14530d
Charlie-Foy-117 <83784786+Charlie-Foy-117@users.noreply.github.com>
2022-09-06 15:52:22 +0100

    function, for, switch

--- CPlusPlusPractice/CPlusPlusPractice.cpp
+++ CPlusPlusPractice/CPlusPlusPractice.cpp
@@ -4,8 +4,14 @@
 #include <iostream>
 #include <string>
 
+void BasicFunction()
+{
+    std::cout << "Basic function ran" << std::endl;
+}
+
 int main()
 {
+    /*
     //examples of different variables
     int testInt = 0;
     unsigned testUnsigned = 100u;
@@ -84,6 +90,7 @@ int main()
 
 
     //selection
+    /*
     bool decision = true;
     if (decision)
     {
@@ -92,14 +99,123 @@ int main()
     else
     {
         std::cout << "Decision was false" << std::endl;
-    }
+    } */
 
     //advanced if statements :Ternary
     //written inline with other code, such as an assignment
     //it results in one of two values based on conditions
 
-    int ternResult = decision ? 1 : 2;
+    //int ternResult = decision ? 1 : 2; 
+    /*
+    int errorCode = 2;
 
+    switch (errorCode)
+    {
+    case 0:
+    {
+        std::cout << "error was 0!" << std::endl;
+        break;
+    }
+    case 1:
+    {
+        std::cout << "error was 1!" << std::endl;
+        break;
+    }
+    case 2:
+    {
+        std::cout << "error was 2!" << std::endl;
+        break;
+    }
+
+    default:
+    {
+        std::cout << "error was unrecongized!" << std::endl;
+        break;
+    }
+    } */
+
+    /*
+    int playerPath = 0;
+
+    std::cout << "Please enter a number that correlates to the path you wish to take" << std::endl
+        << "1. Continue up the rumbling moutain" << std::endl
+        << "2. Move towards the whispering woods" << std::endl
+        << "3. Head across the acient stone bridge" << std::endl
+        << "4. Decide you are tired and go back from where you came from " << std::endl;
+
+    std::cin >> playerPath;
+    switch (playerPath)
+    {
+    case 1:
+    {
+        std::cout << "You have chosen path 1" << std::endl
+            << "You followed a path down towards an unstable volcano and met your end" << std::endl;
+        break;
+    }
+    case 2:
+    {
+        std::cout << "You have chosen path 2" << std::endl
+            << "You stumbled into a dark forest where a passing merchant sells you a berry which when consumed transports you to valhalla" << std::endl;
+        break;
+    }
+    case 3:
+    {
+        std::cout << "You have chosen path 3" << std::endl
+            << "You attempt to cross the bridge but as you are doing so an old grey haired old man shouts at you and destorys the bridge and you fall to your death" << std::endl;
+        break;
+    }
+    case 4:
+    {
+        std::cout << "You have chosen path 4" << std::endl
+            << "You head home and you never have another adventure" << std::endl;
+        break;
+    }
+    default:
+    {
+        std::cout << "You did not enter a correct choice" << std::endl
+            << "You sit down where you are and think about the bad choices you made" << std::endl
+            << "You also get mugged but passing thieves" << std::endl;
+        break;
+    }
+    }
+    */
+
+    /*
+    bool whileBool = true;
+
+    while (whileBool == true)
+    {
+        //do the body of the loop
+        whileBool = false;
+        //at the end if the condtion is still true run loop again
+    }
+
+    int doI = 0;
+    do
+    {
+        ++doI;
+        std::cout << "Do while ran " << doI << std::endl;
+        whileBool = false;
+    } while (doI < 2);
+
+    for ( int i = 0; i < 5; ++i)
+    {
+        std::cout << "for loop ran " << i << " times" << std::endl;
+    }
+    */
+
+    /*
+    std::string google = "G";
+    for (int i = 0; i < 10; ++i)
+    {
+        google += "o";
+    }
+    google += "gle";
+    std::cout << google << std::endl;
+    */
+    
+
+    BasicFunction();
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
